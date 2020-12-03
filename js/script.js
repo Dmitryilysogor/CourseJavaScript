bb415adec93447ad75f0971b0d57dace7f24fe74
Dmitryi <lysogor82@mail.ru>
2020-12-03 17:57:26 +0600

    Initial commit
    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+// console.log(result);
+
+//const answer = prompt("Вам есть 18?", "");
+//console.log(typeof(answer));    
+
+const answers = [];
+
+answers[0] = prompt('Как вашу имя?', '' );
+answers[1] = prompt('Как вашу фамилия?', '' );
+answers[2] = prompt('Сколько вам лет?', '' );
+
+document.write(answers);
+
